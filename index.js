179cce5c067e8dc1bac86fd644cea802bb66931a
R1t1sh <122475010+R1t1sh@users.noreply.github.com>
2023-06-16 13:42:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,17 +21,16 @@ function switchTab(newTab) {
         oldTab.classList.add("current-tab");
 
         if(!searchForm.classList.contains("active")) {
-            //kya search form wala container is invisible, if yes then make it visible
+            //if search form container is invisible, if yes then make it visible
             userInfoContainer.classList.remove("active");
             grantAccessContainer.classList.remove("active");
             searchForm.classList.add("active");
         }
         else {
-            //main pehle search wale tab pr tha, ab your weather tab visible karna h 
+            
             searchForm.classList.remove("active");
             userInfoContainer.classList.remove("active");
-            //ab main your weather tab me aagya hu, toh weather bhi display karna poadega, so let's check local storage first
-            //for coordinates, if we haved saved them there.
+            
             getfromSessionStorage();
         }
     }
